40ba8d2fc0e05f500cef7afc1f0b894ae2b49029
stan <amare605@gmail.com>
2022-09-13 17:15:31 +0800

    增加Rating component  評論星星

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import  Rating  from "./Rating";
 
 import React from 'react'
 
@@ -16,9 +17,7 @@ function Product({product}) {
                 </Card.Title>                
             </a>           
             <Card.Text as="div">
-                <div className="my-3">
-                    {product.rating} from {product.numReviews} Reviews
-                </div>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             </Card.Text>
             <Card.Text as="h3">
                 ${product.price}

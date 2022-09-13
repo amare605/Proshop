80e07a97035ed74a5d5e3b8bc9a38449b2719fe8
stan <amare605@gmail.com>
2022-09-13 14:35:09 +0800

    task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 
+import {Container}  from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header />
+      <main>
+        <Container className="py-3">
+          <h1>Welcome to proshop</h1>  
+        </Container>         
+      </main>
+      <Footer />
     </>
   );
 }
